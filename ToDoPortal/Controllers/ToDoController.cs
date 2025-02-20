73fd9980dbfe89974fa501243453748b3ad4dfd0
Vishal <vishal.u.chinchane@softmass.co.in>
2025-02-20 16:56:15 +0530

    Added Post method to store data inside database

--- ToDoPortal/Controllers/ToDoController.cs
+++ ToDoPortal/Controllers/ToDoController.cs
@@ -1,12 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ToDoPortal.Data;
+using ToDoPortal.Models;
+using ToDoPortal.Models.Entities;
 
 namespace ToDoPortal.Controllers
 {
     public class ToDoController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+        public ToDoController(ApplicationDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ToDoViewModel viewModel)
+        {
+            var todo = new ToDo
+            {
+                Title = viewModel.Title,
+                Description = viewModel.Description,
+                Date = viewModel.Date,
+                isImportant = viewModel.isImportant
+            };
+            await _dbContext.ToDos.AddAsync(todo);
+            await _dbContext.SaveChangesAsync();
+
+
             return View();
         }
     }
---
+++ ToDoPortal/Models/ToDoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoPortal.Models
+{
+    public class ToDoViewModel
+    {
+        public string? Title { get; set; } // this is my primary key
+        public string? Description { get; set; }
+        public DateOnly? Date { get; set; }
+        public bool? isImportant { get; set; } = false;
+    }
+}
